c76cd87fc0dfa47cf8b141b8dbec725f0f0dda16
Chandani <nittianchandani@gmail.com>
2026-06-07 19:12:22 +0530

    Strings basics

---
+++ 2.py
@@ -0,0 +1,4 @@
+# print("%s to the right"%('This is a string'))
+# print('This is my first day of python learining ' end ='' ) # end is used for concatenate two print statement
+# print('This is second day of pyhon')
+print("LOVE U Mumma & Papa  " *10)
