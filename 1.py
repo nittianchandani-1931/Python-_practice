107de8c8cc270bb7dbdcda7a06fb589826f0931a
Chandani <nittianchandani@gmail.com>
2026-06-07 19:02:21 +0530

    Basics of python

---
+++ 1.py
@@ -0,0 +1,17 @@
+# print("Hello baby")
+#Hey i m fine baby , what about u ?
+# '''This is also comments in python called multi-line comments'''
+# cv ="My life line best decision"
+# DOM = 31 
+# LDR = 4
+#Arithmetic  Operrations
+print (" The value of 31 + 19 = " , 31 + 19)
+print(" The value of 31 - 19 = " , 31 - 19)
+print (" The value of 31 * 19 = " , 31 * 19)
+print (" The value of 31 / 19 = " , 31 / 19)
+print (" The value of 31 ** 19 = " , 31 ** 19)
+print (" The value of 31 // 19 = " , 31 // 19)   # fluid divison
+print("This my love \"")
+
+mls ='''This is multiline string and this will keep going and going'''
+print(mls)
